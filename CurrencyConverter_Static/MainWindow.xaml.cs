c79878d2006b9d13baf16cc59bd95e1e725c5eed
Ntuthuzelo <gwintantuthuzelo@gmail.com>
2025-04-28 17:38:52 +0200

    Binding Values to Combo Boxes, Input Validations and Completing the Logic

--- CurrencyConverter_Static/MainWindow.xaml.cs
+++ CurrencyConverter_Static/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,22 +25,126 @@ namespace CurrencyConverter_Static
         public MainWindow()
         {
             InitializeComponent();
-            lblCurrency.Content = "Hello World";
+            BindCurrency();
+        }
+
+        private void BindCurrency()
+        {
+            DataTable dtCurrency = new DataTable();
+            dtCurrency.Columns.Add("Text");
+            dtCurrency.Columns.Add("Value");
+
+            //Add rows in the Database with text and value
+            dtCurrency.Rows.Add("--SELECT--", 0);
+            dtCurrency.Rows.Add("ZAR", 1);
+            dtCurrency.Rows.Add("USD", 18.577789);
+            dtCurrency.Rows.Add("EUR", 21.082839);
+            dtCurrency.Rows.Add("POUND", 24.800229);
+            dtCurrency.Rows.Add("INR", 0.217629);
+            dtCurrency.Rows.Add("AUD", 11.890262);
+            dtCurrency.Rows.Add("CAD", 13.387423);
+            dtCurrency.Rows.Add("SGD", 14.122394);
+            dtCurrency.Rows.Add("CHF", 22.441976);
+            dtCurrency.Rows.Add("MYR", 4.245746);
+            dtCurrency.Rows.Add("JPY", 0.129768);
+
+            cmbFromCurrency.ItemsSource = dtCurrency.DefaultView;
+            cmbFromCurrency.DisplayMemberPath = "Text";
+            cmbFromCurrency.SelectedValuePath = "Value";
+            cmbFromCurrency.SelectedValue = 0;
+
+            cmbToCurrency.ItemsSource = dtCurrency.DefaultView;
+            cmbToCurrency.DisplayMemberPath = "Text";
+            cmbToCurrency.SelectedValuePath = "Value";
+            cmbToCurrency.SelectedValue = 0;
         }
 
         private void Convert_Click(object sender, RoutedEventArgs e)
         {
-            lblCurrency.Content = "Hello Button Clicker";
+            //Create a variable as ConvertedValue with double data type to store currency converted value
+            double ConvertedValue;
+
+            //Check amount textbox is Null or Blank
+            if (txtCurrency.Text == null || txtCurrency.Text.Trim() == "")
+            {
+                //If amount textbox is Null or Blank it will show the below message box   
+                MessageBox.Show("Please Enter Currency", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                //After clicking on message box OK sets the Focus on amount textbox
+                txtCurrency.Focus();
+                return;
+            }
+            //Else if the currency from is not selected or it is default text --SELECT--
+            else if (cmbFromCurrency.SelectedValue == null || cmbFromCurrency.SelectedIndex == 0)
+            {
+                //It will show the message
+                MessageBox.Show("Please Select Currency From", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                //Set focus on From Combobox
+                cmbFromCurrency.Focus();
+                return;
+            }
+            //Else if Currency To is not Selected or Select Default Text --SELECT--
+            else if (cmbToCurrency.SelectedValue == null || cmbToCurrency.SelectedIndex == 0)
+            {
+                //It will show the message
+                MessageBox.Show("Please Select Currency To", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                //Set focus on To Combobox
+                cmbToCurrency.Focus();
+                return;
+            }
+            //If From and To Combobox selected values are same
+            if (cmbFromCurrency.Text == cmbToCurrency.Text)
+            {
+                //The amount textbox value set in ConvertedValue.
+                //double.parse is used to convert datatype String To Double.
+                //Textbox text have string and ConvertedValue is double datatype
+                ConvertedValue = double.Parse(txtCurrency.Text);
+
+                //Show in label converted currency and converted currency name.
+                // and ToString("N3") is used to place 000 after after the(.)
+                lblCurrency.Content = cmbToCurrency.Text + " " + ConvertedValue.ToString("N3");
+            }
+            else
+            {
+
+                //Calculation for currency converter is From Currency value multiply(*) 
+                // with amount textbox value and then the total is divided(/) with To Currency value
+                ConvertedValue = (double.Parse(cmbFromCurrency.SelectedValue.ToString()) * 
+                    double.Parse(txtCurrency.Text)) / 
+                    double.Parse(cmbToCurrency.SelectedValue.ToString());
+
+                //Show in label converted currency and converted currency name.
+                lblCurrency.Content = cmbToCurrency.Text + " " + ConvertedValue.ToString("N3");
+            }
         }
 
         private void Clear_Click(object sender, RoutedEventArgs e)
         {
-            lblCurrency.Content = "";
+            ClearControls();
         }
+
 
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
 
+        private void ClearControls()
+        {
+            txtCurrency.Text = string.Empty;
+            if (cmbFromCurrency.Items.Count > 0)
+            {
+                cmbFromCurrency.SelectedIndex = 0;
+            }
+            if (cmbToCurrency.Items.Count > 0)
+            {
+                cmbToCurrency.SelectedIndex = 0;
+            }
+            lblCurrency.Content = "";
+            txtCurrency.Focus();
         }
     }
 }
